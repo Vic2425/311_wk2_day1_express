ec624f73885c291c4dd02c306794d6073c8981c1
Vic <vicjs612@gmail.com>
2023-01-14 19:16:40 -0600

    make some changes on functions

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ app.get('/users/1', (req, res) => {
 })
 
 app.get('/users/:userId', (req, res) => {
-  let id = req.params.userId -1;
-  res.json(users[id]);
+  res.json(users.find(user => user._id === parseInt(req.params.userId)));
 })
 
 app.post('/users', (req, res) => {
@@ -34,34 +33,34 @@ app.post('/users', (req, res) => {
 })
 
 app.post('/users', (req, res) => {
-  
-  let newUser = {
-    _id: ++counter,
-    name: req.body.name,
-    occupation: req.body.occupation,
-    avatar: req.body.avatar
-  };
-
-  users.push(newUser);
-  // counter++;
+  users.push({
+    _id: counter++,
+    ...req.body    
+  });
   res.json(users[users.length -1])
 })
 
-app.put('/users/1', (req, res) => {
-  users[0].name = 'Danny Dale Cooper'
-  res.json(users);
-})
+// app.put('/users/1', (req, res) => {
+//   users[0].name = 'Danny Dale Cooper'
+//   res.json(users);
+// })
 
 app.put('/users/:userId', (req, res) => {
-  let id = req.params.userId -1;
-  // users[id].name = "Jane Doe";
-  // res.json(users[id])
-  let putUser = req.body;
-  let n = users.findIndex(m => m.userId == id) //get index of members array     
-  users[n].name = putUser.name ? putUser.name : users[n].name;
-  users[n].occupation = putUser.occupation ? putUser.occupation : users[n].occupation;
-  users[n].avatar = putUser.avatar ? putUser.avatar : users[n].avatar;
-  res.json(users[n]);
+  // let id = req.params.userId -1;
+ 
+  // let putUser = req.body;
+  // let n = users.findIndex(m => m.userId == id) //get index of members array     
+  // users[n].name = putUser.name ? putUser.name : users[n].name;
+  // users[n].occupation = putUser.occupation ? putUser.occupation : users[n].occupation;
+  // users[n].avatar = putUser.avatar ? putUser.avatar : users[n].avatar;
+  // res.json(users[n]);
+  let findUser = (users.find( user => user._id === parseInt(req.params.userId)))
+  let user = findUser[0];
+  user.name = req.body.name ? req.body.name : user.name
+  user.avatar = req.body.avatar ? req.body.avatar : user.avatar
+  user.occupation = req.body.occupation ? req.body.occupation : useroccupation
+  
+  res.json(user);
 })
 
 app.delete('/users/1', (req, res) => {
@@ -70,9 +69,14 @@ app.delete('/users/1', (req, res) => {
 })
 
 app.delete('/users/:userId', (req, res) => {
-  let id = req.params.userId -1;
-  users[id].isActive = 'false';
-  res.send('deleted')
+  let findUser = (users.find( user => user._id === parseInt(req.params.userId)));
+  let user = findUser[0];
+  if (user) {
+    users.isActive = 'false';
+    res.send('deleted')
+  } else {
+    res.status(400).json({ msg : `No member with the id of ${req.params.userId} is found!`})
+  }
 })
 /* END - create routes here */
 
